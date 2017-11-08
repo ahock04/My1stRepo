e2e3621213815666a7d3f50e220f17ac0f0bec5e
ahock04 <33496352+ahock04@users.noreply.github.com>
2017-11-08 14:35:06 -0500

    Update Madlib_Chapter2.py

--- Madlib_Chapter2.py
+++ Madlib_Chapter2.py
@@ -1,10 +1,11 @@
 from MadLib_Chapter1 import *
+import time
 
 print("Chapter 2")
 print("Are you ready to begin your adventures? Yes/No")
 if ready == "Yes":
     print("Okay.")
-    print("My name is " + name1 + ", I have been the protector of " + city1 + "for the past six months.")
+    print("My name is " + name1 + ", I have been the protector of " + city1 + "for the last six months.")
     print("Type a noun.")
     noun11 = input().lower()
     print("Type a verb in the past tense.")
@@ -24,9 +25,11 @@ if ready == "Yes":
     print("When we encountered the evil " + noun11 + ",we " + verb11 + " it.")
     print("We quickly " + verb12 + " away in a " + adj11 + "without looking back.")
     print("When we got back to the " + adj12 "quarters we were " + verb13 + "by our " + name11 ".")
-    print("")
+    print("But, we saw that " + noun11 + " was waiting for us at " + adj12 + "quarters")
 
 elif ready == "No":
     print("Your loss.")
 else:
     print("That is not a valid answer.")
+
+time.sleep(1000)
